373703c759d26eb53f233ca16b09a61e6fbf24ad
Gleb Veklenko <glebveklenko59@gmail.com>
2024-05-31 11:03:29 +0300

    auth

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", protect, contactsRouter);
 app.use("/users", usersRouter);
 
 app.use((_, res) => {
